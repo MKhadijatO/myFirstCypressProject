1403c8ca03f956aae06a92f891c2581a555eae19
MKhadijatO <muhammadkhadijato@gmail.com>
2024-07-07 00:41:41 +0100

    form commit

--- cypress/integration/BDD/form.feature
+++ cypress/integration/BDD/form.feature
@@ -2,7 +2,7 @@ Feature: ProtoCommerce website form Validation
  Application regression​
 @regression
 
-Scenario:Submit form
+Scenario: Submit form
     Given I open ProtoCommerce website
     Then I filled the form
     Then I click Submit
--- cypress/integration/BDD/form/form.js
+++ cypress/integration/BDD/form/form.js
@@ -7,21 +7,27 @@ beforeEach( function(){
     cy.fixture('example').then(function(data)
     {
         this.data =data
-})
+    })
 });
 
 
 Given ('I open ProtoCommerce website', function(){
-    cy.visit('https://rahulshettyacademy.com/angularpractice/')
+    cy.visit('https://rahulshettyacademy.com/angularpractice/');
 }) 
 
 Then ('I filled the form', function(){
-    cy.get('').type('')
-    cy.get('').type('')
-    cy.get('').type('')
-    cy.get('').type('')
-    cy.get('').type('')
-    cy.get('').type('')
+    
+    cy.get("div[class='form-group'] input[name='name']").type("Khadijat");
+    cy.get("input[name='email']").type("muhammadkhadijato@gmail.com");
+    cy.get("#exampleInputPassword1").type("khadijat02.");
+    cy.get("#exampleCheck1").check();
+    cy.get("#exampleFormControlSelect1").select("Female");
+    cy.get("#inlineRadio1").check().should('be.checked')
+    cy.get("input[name='bday']").type('2000-06-07');
+    
 })
 
-Then ('I click Submit',)
+Then ('I click Submit', function(){
+    cy.get("body > app-root > form-comp > div > form").submit();
+    // cy.get("input[value='Submit']").click();
+})
