8fa1829c56ec46030543dbe2fc069b76c646cbf8
MKhadijatO <muhammadkhadijato@gmail.com>
2024-07-20 23:41:40 +0100

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,11 @@
-const { defineConfig } = require("Cypress");
+const { defineConfig } = require("cypress");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
 async function setupNodeEvents(on, config) {
   await preprocessor.addCucumberPreprocessorPlugin(on, config);
   on("file:preprocessor", browserify.default(config));
-  return config,
-  {
-    browsers: [
-      {
-        name: 'firefox',
-        family: 'firefox',
-        channel: 'stable',
-        displayName: 'Firefox',
-        version: execSync('firefox --version').toString().split(' ')[2],
-        path: 'C:\\Program Files\\Mozilla Firefox\\firefox.exe', // Adjust this path based on your OS and Firefox installation path
-        majorVersion: execSync('firefox --version').toString().split(' ')[2].split('.')[0],
-        isHeaded: true,
-        isHeadless: true
-      }
-    ]
-  };
+  return config;
 }
 
 module.exports = defineConfig({
